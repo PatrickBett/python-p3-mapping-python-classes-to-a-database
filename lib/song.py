3ba412bfe014214700bb73e105b239cb1bda2128
PatrickBett <patrickbett018@gmail.com>
2023-12-09 14:47:44 +0300

    First commit

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,39 @@
 from config import CONN, CURSOR
 
+
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.name = name
+        self.album = album
+
+
+
+
+    @classmethod
+    
+    def create_table(self):        
+        sql = '''
+        CREATE TABLE IF NOT EXISTS songs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            album TEXT
+        )
+        '''
+
+        CURSOR.execute(sql)
+
+
+    def save(self):
+        sql = '''
+            INSERT INTO songs( name, album)
+            VALUES(?,?)
+        '''
+        CONN.execute(sql,(self.name, self.album))
+
+
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
